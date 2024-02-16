24a3a993317fe0af8f0635dca5655f03e437a247
DESKTOP-4QG58O2\Admin <rayamirzoyeva@gmail.com>
2024-02-16 12:31:44 +0400

    commit

---
+++ src/Layouts/Body/Body.jsx
@@ -0,0 +1,16 @@
+
+import './body.css';
+
+function Body ({children}) {
+    
+	return(
+
+		<div className='body'> 
+			{children}
+		</div>
+     
+	);
+
+}
+
+export default Body;
---
+++ src/Layouts/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,16 @@
+
+import './LeftPanel.css';
+
+function LeftPanel ({children}) {
+    
+	return(
+
+		<div className='left-panel'> 
+			{children}
+		</div>
+     
+	);
+
+}
+
+export default LeftPanel;
